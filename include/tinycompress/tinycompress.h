1254aebef3ad1ae658180c4984fc4b6d0c6dd58c
Manish Dewangan <manish@codeaurora.org>
2017-01-17 11:57:09 +0530

    tinycompress: squash vendor specific tinycompress changes
    tinycompress: Generate vendor specifc tinycompress
    
    Generate libtinycompress_vendor that contains the
    tinycompress extensions
    
    CRs-Fixed: 2074491
    
    Change-Id: I1466c13f216ce723d63155bc099eccad4fdccdfe
    
    From: Vijay Venkatraman <vijaykv@google.com>
    Date: Tue, 9 May 2017 11:44:14 -0700
    Subject: [PATCH] Move libtinycompress to vendor partition
    
    Bug: 33241851
    Test: Build sailfish and pass CTS audio
    Test: With BOARD_VNDK_VERSION:=current, no link type check errors for
    this lib
    
    (cherry picked from commit c1c913ac024bd59171d13f28127ece9dabe49490)
    
    Merged-In: I28e9d98a6a77a3e3ea67b406d08861216fa2553b
    Change-Id: Ice58fe2ea73188177dde2cb8a30da741ae68cd65
    
    From: Manish Dewangan <manish@codeaurora.org>
    Date: Tue, 17 Jan 2017 11:57:09 +0530
    Subject: [PATCH] tinycompress: Add get_metadata() and set_metadata() API
     support
    
    -Add generic get_metadata() and set_metadata() APIs so that client
     call these API to get/set metadata value based on key.
    
    CRs-Fixed: 2015309
    Change-Id: Id81bd8585ed389efc3d0c830cb6cc1678d4d4e56

--- include/tinycompress/tinycompress.h
+++ include/tinycompress/tinycompress.h
@@ -82,6 +82,7 @@ struct compr_gapless_mdata {
 struct compress;
 struct snd_compr_tstamp;
 union snd_codec_options;
+struct snd_compr_metadata;
 
 /*
  * compress_open: open a new compress stream
@@ -245,8 +246,10 @@ int compress_set_gapless_metadata(struct compress *compress,
  * @codec_options: codec options of compress stream based on codec type
  */
 
+#ifdef USE_VENDOR_EXTN
 int compress_set_next_track_param(struct compress *compress,
 			union snd_codec_options *codec_options);
+#endif
 
 /*
  * is_codec_supported:check if the given codec is supported
@@ -304,6 +307,13 @@ const char *compress_get_error(struct compress *compress);
 /* utility functions */
 unsigned int compress_get_alsa_rate(unsigned int rate);
 
+/* set metadata */
+int compress_set_metadata(struct compress *compress,
+	    struct snd_compr_metadata *mdata);
+
+/* get metadata */
+int compress_get_metadata(struct compress *compress,
+	    struct snd_compr_metadata *mdata);
 
 #if defined(__cplusplus)
 }
