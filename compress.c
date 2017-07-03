d6ad6f1e5310c2b1a974ca3911ecccb0fd4444cd
razorloves <razorloves@gmail.com>
2017-07-03 11:26:29 +0530

    Revert "tinycompress: Generate vendor specifc tinycompress"
    ** Fixes playing mp3 files.
    
    Conflicts:
    	compress.c
    This reverts commit 864114ceee3ff6bb3318743bf660df7d24153e29.
    
    Change-Id: Ibcdc4831d374b55ba42f8d472433621770a14e0f

--- compress.c
+++ compress.c
@@ -572,7 +572,6 @@ int compress_set_gapless_metadata(struct compress *compress,
 	return 0;
 }
 
-#ifdef USE_VENDOR_EXTN
 #if defined(SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM)
 int compress_set_next_track_param(struct compress *compress,
 	union snd_codec_options *codec_options)
@@ -590,7 +589,6 @@ int compress_set_next_track_param(struct compress *compress __unused,
 {
 	return 0;
 }
-#endif
 #endif
 
 bool is_codec_supported(unsigned int card, unsigned int device,
