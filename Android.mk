951ee53c980f0de56347334f0441dbd631810239
razorloves <razorloves@gmail.com>
2018-02-07 19:58:13 -0600

    Revert "libtinycompress: Android.mk -> Android.bp"
    Soong is not including kernel headers, so we need to convert
    back to mk. This fixes mp3 playback on nexus devices.
    
    This reverts commit b09957d4375aea27f2bd297e6dc7878e594fc57e.
    
    Change-Id: If596f1ec378bb124a2da5da964455beece2ab46d

--- Android.mk
+++ Android.mk
@@ -1,7 +1,16 @@
+#Do NOT Convert to Android.bp since this needs to compile with
+#device specific kernel headers, that Soong does not support.
+#https://android-review.googlesource.com/#/q/topic:revert-ltc-soong
+#b/38117654
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS := -Wno-macro-redefined
+LOCAL_CFLAGS := \
+    -Wall \
+    -Werror \
+    -Wno-macro-redefined \
+    -Wno-unused-function
 LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
@@ -12,3 +21,31 @@ LOCAL_SHARED_LIBRARIES:= libcutils libutils
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_CFLAGS := \
+    -Wall \
+    -Werror \
+    -Wno-macro-redefined \
+    -Wno-unused-function
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
+LOCAL_SRC_FILES:= compress.c utils.c
+LOCAL_MODULE := libtinycompress
+LOCAL_SHARED_LIBRARIES:= libcutils libutils
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_CFLAGS := \
+    -Wall \
+    -Werror \
+    -Wno-macro-redefined
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
+LOCAL_SRC_FILES:= cplay.c
+LOCAL_MODULE := cplay
+LOCAL_SHARED_LIBRARIES:= libcutils libutils libtinycompress
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
